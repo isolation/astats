2042a4d254187b456e205ae406dfb4e3fc9c22ee
isolation <lucifer7@gmail.com>
2014-03-14 02:59:33 -0700

    oh THAT'S where i put that file

--- www/sandbox.php
+++
@@ -1,8 +0,0 @@
-<?php
-$password = "1a9KLLASjrahr";
-$iterations = 1000;
-$salt = mcrypt_create_iv(24, MCRYPT_DEV_URANDOM);
-echo bin2hex($salt) . "\n";
-$hashed = hash_pbkdf2("sha512", $password, $salt, $iterations, 128, false);
-echo $hashed;
-?>
